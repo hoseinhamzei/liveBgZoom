354419acceeb2def08d0703836f548155e4e6fd7
Mehrdad Hamzei <hnkn1998@gmail.com>
2018-08-19 17:02:59 +0430

    Update livebg.js
    -fixed bugs in static sized images

--- js/livebg.js
+++ js/livebg.js
@@ -1,22 +1,41 @@
 
 var x,y;
-$(".lbg-container").mousemove(function(event) {
+
+
+$(".lbg-container").each(function() {
+    
+    
     var offset = $(this).offset();
     var width = $(this).width();
     var height = $(this).height();
     
-    x = ((event.pageX- offset.left)/width) * 100;
+    $(this).css({"background-position": "center center","background-size":`${width+height/2}px`});
+    
+    $(this).mousemove(function(event){
+        
+        x = ((event.pageX- offset.left)/width) * 100;
     y = ((event.pageY- offset.top)/height) * 100;
     
     var bgposti = x+"%" + " " + y+"%";
+        
+        
+        /// ENTER CUSTOM ZOOM AMOUNT ///
+        var zoomAmount = "50%";
+        
+        
+    var sum = width+height/2;
+    sum += sum * (parseFloat(zoomAmount)/100);    
+        
+    $(this).css({"background-position": bgposti,"background-size":sum+"px"});
+        
+    });
     
-    //// SET ZOOM AMOUNT HERE ////
-    var zoomAmount = "150%";
     
-    $(this).css({"background-position": bgposti,"background-size":zoomAmount});
-    
+    $(this).mouseleave(function(){
+    $(this).css({"background-position": "center center","background-size":`${width+height/2}px`});
 });
-
-$(".lbg-container").mouseleave(function(){
-    $(this).css({"background-position": "50% 50%","background-size":"100%"});
+    
+    
+    
+    
 });
